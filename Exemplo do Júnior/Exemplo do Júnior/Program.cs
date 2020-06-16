f1fd2675c1aab6d4d9326e3fd56ef5e55df32873
Kayky933 <kayky7277@gmail.com>
2020-06-16 12:32:25 -0300

    Arquivos novos

---
+++ Exemplo do Júnior/Exemplo do Júnior/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo_do_Júnior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
