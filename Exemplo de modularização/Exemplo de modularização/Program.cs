beb4cc06be2d4c1891cdf144e724e93ffe80b749
Kayky933 <kayky7277@gmail.com>
2020-07-07 13:11:00 -0300

    Arquivos Visual Studio 2017

--- Exemplo de modularização/Exemplo de modularização/Program.cs
+++ Exemplo de modularização/Exemplo de modularização/Program.cs
@@ -3,32 +3,33 @@
 namespace Exemplo_De_Modularização
 {
 
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			double salario, salarioLiquido, IR, INSS, VT;
-			Console.WriteLine("Informe o valor do salário: ");
-			salario = Convert.ToDouble(Console.ReadLine());
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, salarioLiquido, IR, INSS, VT;
+            Console.WriteLine("Informe o valor do salário: ");
+            salario = Convert.ToDouble(Console.ReadLine());
 
-			IR = calcular(salario, 0.15);
-			INSS = calcular(salario, 0.11);
-			VT = calcular(salario, 0.20);
+            IR = calcular(salario, 0.15);
+            INSS = calcular(salario, 0.11);
+            VT = calcular(salario, 0.20);
 
-			salarioLiquido = salario - IR - INSS - VT;
+            salarioLiquido = salario - IR - INSS - VT;
 
-			Console.WriteLine("Valor do inposto de renda: " + IR +
-							  "\n.Valor do imposto do INSS: " + INSS +
-							  "\n.Valor do Vale Transporte " + VT +
-							  "\n.Valor do salário a receber: " + salarioLiquido);
+            Console.WriteLine("Valor do inposto de renda: " + IR +
+                              "\n.Valor do imposto do INSS: " + INSS +
+                              "\n.Valor do Vale Transporte " + VT +
+                              "\n.Valor do salário a receber: " + salarioLiquido);
+            Console.ReadKey();
 
-			double calcular(double salario, double aliquota)
-			{
-				double calculo;
-				calculo = salario * aliquota;
-				return calculo;
-			}
+            double calcular(double salar, double aliquota)
+            {
+                double calculo;
+                calculo = salario * aliquota;
+                return calculo;
+            }
 
-		}
-	}
+        }
+    }
 }
---
+++ Hellow World/Hellow World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exemplo_do_Júnior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
